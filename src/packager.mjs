fb268f663a719853e1889106ba10a6b3a438e63e
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-12-07 00:35:12 +0100

    Work in progress...

--- src/packager.mjs
+++ src/packager.mjs
@@ -2,40 +2,57 @@ import fs from "./customFS.mjs";
 import { installPackager as InstallPackager } from "../res/packager/install.mjs";
 import Ffmpeg, { installFFMPEG } from "./ffmpeg.mjs";
 
+import { flat } from "./polyfils.mjs";
+Array.prototype.flat = function() {
+  return flat(this[0]);
+};
 export const installPackager = InstallPackager;
 
 class Packager {
   constructor(verbose = false) {
     this.verbose = verbose;
     this.inputs = [];
-    this.choosenProfile = this.profiles["on-demand"];
+    this.choosenProfile = Packager.profiles["on-demand"];
     this.choosenMinimumBufferTime = 3;
     this.choosenSegmentDuration = 3;
     if (verbose) {
       console.log("Initializing new Packager.");
     }
   }
-  get profiles() {
+  static get profiles() {
     return Object.freeze({ "on-demand": "on-demand" });
   }
-  input(file) {
-    const resolution = getResolution(file);
-    this.inputs.push({
-      resolution,
-      fileName: fs.getFileName(file),
-      filePath: file
-    });
-    if (this.verbose) {
-      console.log(`Added input file: ${file}.`);
+  static get qualities() {
+    return Object.freeze({ "720p": "720p", "540p": "540p", "360p": "360p" });
+  }
+  input(file, packagerQualities = []) {
+    packagerQualities = flat(packagerQualities);
+    let passedCheck = true;
+    for (let packagerQuality of packagerQualities) {
+      if (!Object.values(Packager.qualities).includes(packagerQuality)) {
+        passedCheck = false;
+        throw new UnknownPackageQualityError(packagerQualities);
+      }
+    }
+    if (passedCheck) {
+      const resolution = getResolution(file);
+      this.inputs.push({
+        resolution,
+        fileName: fs.getFileName(file),
+        filePath: file
+      });
+      if (this.verbose) {
+        console.log(`Added input file: ${file}.`);
+      }
     }
   }
-  set profile(type) {
-    if (!Object.values(this.profiles).includes(type)) {
-      throw new UnknownPackageProfileError(type);
+  set profile(packagerProfiles) {
+    if (!Object.values(Packager.profiles).includes(packagerProfiles)) {
+      throw new UnknownPackageProfileError(packagerProfiles);
     } else {
-      this.choosenProfile = type;
+      this.choosenProfile = packagerProfiles;
       if (this.verbose) {
-        console.log(`Changed profile to: ${type}.`);
+        console.log(`Changed profile to: ${packagerProfiles}.`);
       }
     }
   }
@@ -90,15 +107,31 @@ async function getResolution(file) {
   }
 }
 
-class UnknownPackageProfileError extends Error {
-  constructor(type) {
+class UnknownPackageError extends Error {
+  constructor(
+    objectValue = "y",
+    objectName = "X",
+    invocationDescription = "X.key"
+  ) {
     super(
-      `Profile of type: ${type} is unknown. Please use a profile that's accessible trough new Packager().profiles.`
+      `${objectName} of type: ${objectValue} is unknown. Please use a ${objectName} that's accessible trough ${invocationDescription}.`
     );
+  }
+}
+
+class UnknownPackageProfileError extends UnknownPackageError {
+  constructor(objectValue) {
+    super(objectValue, "Profile", "Packager.profiles;");
+  }
+}
+
+class UnknownPackageQualityError extends UnknownPackageError {
+  constructor(objectValue) {
+    super(objectValue, "Quality", "Packager.qualities;");
   }
 }
 const packager = new Packager("");
 packager.input(
-  "C:\\Users\\maras\\Desktop\\streaming.service\\res\\videos\\series\\pl\\naruto\\season_3\\naruto_boruto_134.mp4"
+  "C:\\Users\\Patryk Sitko\\projecty\\streaming.service\\res\\video.mp4"
 );
 packager.save("packager1");
