8ff2d418cc8a1a485af93f0bfd181b436b5a6051
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-12-06 17:39:46 +0100

    Fixed getFileName and moved async in addUserEntry

--- src/customFS.mjs
+++ src/customFS.mjs
@@ -48,50 +48,52 @@ function copyFolderRecursiveSync(source, target) {
  * @param invocationCommand The command that will be used in the command-line to invoke the application that corresponds to the new path entry.
  * @returns {Promise} true when adding new user path entry is terminated.
  */
-async function addUserPathEntry(newPathEntry, invocationCommand) {
-  return new Promise(resolve=>{try {
-    const { stdout: userPath } = await exec(
-      '%SystemRoot%\\System32\\reg.exe query "HKCU\\Environment" /v Path'
-    );
-    const localPath = userPath
-      .replace("HKEY_CURRENT_USER\\Environment", "")
-      .replace("Path    REG_EXPAND_SZ", "")
-      .trim();
+function addUserPathEntry(newPathEntry, invocationCommand) {
+  return new Promise(async resolve => {
+    try {
+      const { stdout: userPath } = await exec(
+        '%SystemRoot%\\System32\\reg.exe query "HKCU\\Environment" /v Path'
+      );
+      const localPath = userPath
+        .replace("HKEY_CURRENT_USER\\Environment", "")
+        .replace("Path    REG_EXPAND_SZ", "")
+        .trim();
 
-    console.log("Adding new user path entry: " + newPathEntry);
-    if (!localPath.includes(newPathEntry)) {
-      const { stdout } = await exec(`setx path "${localPath}${newPathEntry}"`);
-      console.log(stdout);
-      console.log("Finnished adding new user path entry: " + newPathEntry);
-      await question.askToReboot();
-    } else {
-      console.log("UserPath already contains path: " + newPathEntry);
-      console.log("Abording adding new user path entry.");
-      try {
-        await exec(invocationCommand);
-      } catch ({ stderr }) {
-        if (
-          stderr ===
-          `'${invocationCommand}' is not recognized as an internal or external command,\r\n" +
+      console.log("Adding new user path entry: " + newPathEntry);
+      if (!localPath.includes(newPathEntry)) {
+        const { stdout } = await exec(
+          `setx path "${localPath}${newPathEntry}"`
+        );
+        console.log(stdout);
+        console.log("Finnished adding new user path entry: " + newPathEntry);
+        await question.askToReboot();
+      } else {
+        console.log("UserPath already contains path: " + newPathEntry);
+        console.log("Abording adding new user path entry.");
+        try {
+          await exec(invocationCommand);
+        } catch ({ stderr }) {
+          if (
+            stderr ===
+            `'${invocationCommand}' is not recognized as an internal or external command,\r\n" +
             "operable program or batch file.\r\n`
-        ) {
-          console.log("Command:", stderr.trim());
-          console.log("path variable need to be updated.");
-          await question.askToReboot();
+          ) {
+            console.log("Command:", stderr.trim());
+            console.log("path variable need to be updated.");
+            await question.askToReboot();
+          }
         }
+        await question.continueOnInput();
       }
-      await question.continueOnInput();
+    } catch (err) {
+      console.error(err);
     }
-  } catch (err) {
-    console.error(err);
-  }
-  resolve(true);
-});
-  
+    resolve(true);
+  });
 }
 
-function getFileName(filepath) {
-  if (fs.isDirectory(filepath)) {
+async function getFileName(filepath) {
+  if ((await fs.promises.lstat(filepath)).isDirectory()) {
     return undefined;
   } else {
     return filepath.slice(filepath.lastIndexOf("\\") + 1, filepath.length);
