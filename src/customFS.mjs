eb69c9750bc36cecd4b04662a176bff27066aa21
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-12-06 14:49:36 +0100

    Added addUserPathEntry function

--- src/customFS.mjs
+++ src/customFS.mjs
@@ -1,5 +1,10 @@
 import fs from "fs";
+import util from "util";
 import path from "path";
+import question from "./questions.mjs";
+import child_process from "child_process";
+
+const exec = util.promisify(child_process.exec);
 
 function copyFileSync(source, target) {
   let targetFile = target;
@@ -36,4 +41,52 @@ function copyFolderRecursiveSync(source, target) {
     });
   }
 }
-export default { ...fs, copyFileSync, copyFolderRecursiveSync };
+/**
+ *
+ * @param newPathEntry The path entry you would like to add to the local user environment variables.
+ * @param invocationCommand The command that will be used in the command-line to invoke the application that corresponds to the new path entry.
+ */
+async function addUserPathEntry(newPathEntry, invocationCommand) {
+  try {
+    const { stdout: userPath } = await exec(
+      '%SystemRoot%\\System32\\reg.exe query "HKCU\\Environment" /v Path'
+    );
+    const localPath = userPath
+      .replace("HKEY_CURRENT_USER\\Environment", "")
+      .replace("Path    REG_EXPAND_SZ", "")
+      .trim();
+
+    console.log("Adding new user path entry: " + newPathEntry);
+    if (!localPath.includes(newPathEntry)) {
+      const { stdout } = await exec(`setx path "${localPath}${newPathEntry}"`);
+      console.log(stdout);
+      console.log("Finnished adding new user path entry: " + newPathEntry);
+      await question.askToReboot();
+    } else {
+      console.log("UserPath already contains path: " + newPathEntry);
+      console.log("Abording adding new user path entry.");
+      try {
+        await exec(invocationCommand);
+      } catch ({ stderr }) {
+        if (
+          stderr ===
+          `'${invocationCommand}' is not recognized as an internal or external command,\r\n" +
+            "operable program or batch file.\r\n`
+        ) {
+          console.log("Command:", stderr.trim());
+          console.log("path variable need to be updated.");
+          await question.askToReboot();
+        }
+      }
+      await question.continueOnInput();
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+export default {
+  ...fs,
+  copyFileSync,
+  copyFolderRecursiveSync,
+  addUserPathEntry
+};
