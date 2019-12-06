2c991f9aa8de922d5d1066c6f9a69da084587f30
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-12-06 17:22:51 +0100

    Made installation process return true when done.

--- res/ffmpeg/install.mjs
+++ res/ffmpeg/install.mjs
@@ -6,26 +6,41 @@ import { dirname } from "path";
 const homeDir = process.env.HOMEDRIVE + process.env.HOMEPATH;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+/**@returns {Promise} true when installing ffmpeg is terminated. */
 export async function installFFMPEG() {
-  const startFFMPEGInstallation = await question.yesNoQuestion(
+  return new Promise(resolve=>{const startFFMPEGInstallation = await question.yesNoQuestion(
     "Do you want to start the ffmpeg installation process?"
   );
   if (startFFMPEGInstallation) {
-    copyFfmpegToHomeDir();
-    fs.addUserPathEntry(`${homeDir}\\ffmpeg\\bin;`, "ffmpeg");
+    let addedUserPathEntry = false;
+    const finnishedCopyingFFMPEG = await copyFfmpegToHomeDir();
+    if (finnishedCopyingFFMPEG) {
+      addedUserPathEntry = await fs.addUserPathEntry(
+        `${homeDir}\\ffmpeg\\bin;`,
+        "ffmpeg"
+      );
+    }
+    if (addedUserPathEntry) {
+      resolve(true);
+    }
   } else {
     console.log("Missing ffmpeg. Exiting process with code 1.");
     await question.continueOnInput(1);
-  }
+    resolve(true);
+  }});
 }
 
+/**@returns {Promise} true when copying ffmpeg to home dir is finnished. */
 function copyFfmpegToHomeDir() {
-  console.log("Starting ffmpeg installation process...");
-  console.log(
-    `Copying files and directories from: "${__dirname}\\**" to: "${homeDir}\\ffmpeg"`
-  );
-  fs.copyFolderRecursiveSync(__dirname, homeDir);
-  console.log(
-    `Finnished copying files and directories from: "${__dirname}\\**" to: "${homeDir}\\ffmpeg"`
-  );
+  return new Promise(resolve => {
+    console.log("Starting ffmpeg installation process...");
+    console.log(
+      `Copying files and directories from: "${__dirname}\\**" to: "${homeDir}\\ffmpeg"`
+    );
+    fs.copyFolderRecursiveSync(__dirname, homeDir);
+    console.log(
+      `Finnished copying files and directories from: "${__dirname}\\**" to: "${homeDir}\\ffmpeg"`
+    );
+    resolve(true);
+  });
 }
--- res/packager/install.mjs
+++ res/packager/install.mjs
@@ -2,30 +2,44 @@ import fs from "../../src/customFS.mjs";
 import question from "../../src/questions.mjs";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import { resolve } from "dns";
 
 const homeDir = process.env.HOMEDRIVE + process.env.HOMEPATH;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+/**@returns {Promise} true when installing packager is terminated. */
 export async function installPackager() {
+  return new Promise(resolve=>{
   const startPackagerInstallation = await question.yesNoQuestion(
     "Do you want to start the packager installation process?"
   );
   if (startPackagerInstallation) {
-    copyPackagerToHomeDir();
-    fs.addUserPathEntry(`${homeDir}\\packager\\bin;`, "packager-win");
+    let addedUserPathEntry = false;
+    const finnishedCopyingFFMPEG = await copyPackagerToHomeDir();
+    if (finnishedCopyingFFMPEG) {
+      addedUserPathEntry = await fs.addUserPathEntry(`${homeDir}\\packager\\bin;`, "packager-win");
+    }
+      if (addedUserPathEntry) {
+        resolve(true);
+      }
   } else {
     console.log("Missing packager. Exiting process with code 1.");
     await question.continueOnInput(1);
-  }
+    resolve(true);
+  }});
 }
 
+/**@returns {Promise} true when copying packager to home dir is finnished. */
 function copyPackagerToHomeDir() {
-  console.log("Starting packager installation process...");
-  console.log(
-    `Copying files and directories from: "${__dirname}\\**" to: "${homeDir}\\packager"`
-  );
-  fs.copyFolderRecursiveSync(__dirname, homeDir);
-  console.log(
-    `Finnished copying files and directories from: "${__dirname}\\**" to: "${homeDir}\\packager"`
-  );
+  return new Promise(resolve => {
+    console.log("Starting packager installation process...");
+    console.log(
+      `Copying files and directories from: "${__dirname}\\**" to: "${homeDir}\\packager"`
+    );
+    fs.copyFolderRecursiveSync(__dirname, homeDir);
+    console.log(
+      `Finnished copying files and directories from: "${__dirname}\\**" to: "${homeDir}\\packager"`
+    );
+    resolve(true);
+  });
 }
