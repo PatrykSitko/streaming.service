181ff55e47c55e088aecd90bbe7a11c476469216
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-12-11 04:32:09 +0100

    Binded the redux socketIO connection listener

--- client/src/redux/store/listeners/bind.js
+++ client/src/redux/store/listeners/bind.js
@@ -1,7 +1,9 @@
 import bindWindowListeners from "./listener/window";
 import bindHistoryListener from "./listener/history";
+import bindSocketIOConnections from "./listener/socketIO/connections";
 
 export default store => {
   bindHistoryListener(store);
   bindWindowListeners(store);
+  bindSocketIOConnections(store);
 };
