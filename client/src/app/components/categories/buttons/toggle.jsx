8d210651a398ffec1d6e68bcd26ce53efc7adaf3
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-12-11 04:24:23 +0100

    Created a toggle button component

---
+++ client/src/app/components/categories/buttons/toggle.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./toggle.scss";
+
+function CategoriesToggleButton() {
+  return <div className="categories-button">Categories</div>;
+}
+
+export default CategoriesToggleButton;
